188409e3832164256fada05bf196ad45fc2cae74
codedfingers <alexyikeh@gmail.com>
2024-10-24 13:04:43 +0100

    Add: instrument app with open telemetry collector

--- src/otel.js
+++ src/otel.js
@@ -1,12 +1,21 @@
-const { NodeSDK } = require('@opentelemetry/sdk-node');
-const { CollectorTraceExporter } = require('@opentelemetry/exporter-collector');
+// src/otel.js
+import { WebTracerProvider } from '@opentelemetry/tracing';
+import { CollectorTraceExporter } from '@opentelemetry/exporter-collector';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
 
-const sdk = new NodeSDK({
-  traceExporter: new CollectorTraceExporter({
-    url: 'http://otel-collector:4317', // Use the gRPC port for tracing
-  }),
+const exporter = new CollectorTraceExporter({
+  url: 'http://otel-collector:4317', // Use your collector's IP
 });
 
-sdk.start()
-  .then(() => console.log('OpenTelemetry initialized'))
-  .catch((err) => console.log('Error initializing OpenTelemetry', err));
+const provider = new WebTracerProvider({
+  resource: {
+    // Set the service name to identify your application
+    service: 'kito',
+  },
+});
+
+provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+provider.register();
+registerInstrumentations({
+  tracerProvider: provider,
+});
